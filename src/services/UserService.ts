5fd7a9631f60778aecb698dcaf7603fb6af05321
bagusiqbal_ <bagus.iqbal@ailima.co.id>
2026-04-24 22:31:19 +0700

    Connect frontend with api, starting with login and register

--- src/services/UserService.ts
+++ src/services/UserService.ts
@@ -1,13 +1,21 @@
 import { User } from "@/src/models/User";
-import { IUserResponse, IUserCreateRequest, IUserUpdateRequest } from "@/src/types/IUserResponse";
+import {
+  IUserResponse,
+  IUserCreateRequest,
+  IUserUpdateRequest,
+  IRegisterResponse,
+} from "@/src/types/IUserResponse";
 
 export class UserService {
   private baseUrl = process.env.NEXT_PUBLIC_API_URL
-    ? `${process.env.NEXT_PUBLIC_API_URL}/account`
+    ? `${process.env.NEXT_PUBLIC_API_URL}/accounts`
     : "http://localhost:8080/account";
 
+  private REGISTER_ENDPOINT: string = `http://localhost:8080/accounts`;
+  private LOGIN_ENDPOINT: string = `http://localhost:8080/sessions`;
+
   async loginAPI(username: string, password: string): Promise<User> {
-    const res = await fetch(`${this.baseUrl}/login`, {
+    const res = await fetch(this.LOGIN_ENDPOINT, {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ username, password }),
@@ -27,8 +35,8 @@ export class UserService {
     return new User(rawData);
   }
 
-  async registerAPI(payload: IUserCreateRequest): Promise<User> {
-    const res = await fetch(this.baseUrl, {
+  async registerAPI(payload: IUserCreateRequest): Promise<IRegisterResponse> {
+    const res = await fetch(this.REGISTER_ENDPOINT, {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(payload),
@@ -36,8 +44,8 @@ export class UserService {
 
     if (!res.ok) throw new Error("Registrasi gagal");
 
-    const rawData: IUserResponse = await res.json();
-    return new User(rawData);
+    const rawData: IRegisterResponse = await res.json();
+    return rawData;
   }
 
   async updateUser(id: number, payload: IUserUpdateRequest): Promise<User> {
